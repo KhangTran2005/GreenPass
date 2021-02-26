77520ab2777d89b6d1b59a81d70792ad25a00777
Spongiforma <56219994+Spongiforma@users.noreply.github.com>
2021-02-26 20:05:32 +0800

    Fix incorrect app id

--- app/src/main/java/com/example/greenpass/MainActivity.kt
+++ app/src/main/java/com/example/greenpass/MainActivity.kt
@@ -14,6 +14,7 @@ import androidx.drawerlayout.widget.DrawerLayout
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.Toolbar
 import kotlinx.android.synthetic.main.app_bar_main.*
+import org.mindrot.jbcrypt.BCrypt
 
 class MainActivity : AppCompatActivity() {
 
