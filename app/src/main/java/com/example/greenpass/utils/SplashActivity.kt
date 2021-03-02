c15fb43120f931d8865c8848694e4a3309995a28
Tran Duc Khang <79127891+KhangTran2005@users.noreply.github.com>
2021-03-02 15:06:58 +0800

    added crappy splash screen

---
+++ app/src/main/java/com/example/greenpass/utils/SplashActivity.kt
@@ -0,0 +1,31 @@
+package com.example.greenpass.utils
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.WindowManager
+import android.view.animation.AnimationUtils
+import com.example.greenpass.MainActivity
+import com.example.greenpass.R
+import kotlinx.android.synthetic.main.activity_splash.*
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+
+@Suppress("DEPRECATION")
+class SplashActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        window.setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN)
+        setContentView(R.layout.activity_splash)
+
+        greenpass_img.animation = AnimationUtils.loadAnimation(this, R.anim.from_top_splash)
+        greenpass_label.animation = AnimationUtils.loadAnimation(this, R.anim.item_slide_from_bottom)
+
+        GlobalScope.launch {
+            delay(2000L)
+            startActivity(Intent(this@SplashActivity, MainActivity::class.java))
+            finish()
+        }
+    }
+}
