a73de5c46d40b34795efba52190c19c13be83d6f
Spongiforma <56219994+Spongiforma@users.noreply.github.com>
2021-03-15 20:09:14 +0800

    Updated splash screen with spaghetti code

--- app/src/main/java/com/example/greenpass/ui/base/SplashActivity.kt
+++ app/src/main/java/com/example/greenpass/ui/base/SplashActivity.kt
@@ -5,9 +5,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.view.WindowManager
-import android.view.animation.AccelerateInterpolator
-import android.view.animation.AnimationUtils
-import android.view.animation.TranslateAnimation
+import android.view.animation.*
 import com.example.greenpass.MainActivity
 import com.example.greenpass.R
 import com.example.greenpass.utils.Rotate3dAnimation
@@ -26,8 +24,12 @@ class SplashActivity : AppCompatActivity() {
 //        greenpass_img.animation = AnimationUtils.loadAnimation(this, R.anim.from_top_splash)
 //        greenpass_label.animation = AnimationUtils.loadAnimation(this, R.anim.from_bottom_splash)
 
-        gate_rotation(heaven_gate_left,true,35f)
-        gate_rotation(heaven_gate_right,false,35f)
+        gateRotation(heaven_gate_left,true,35f)
+        gateRotation(heaven_gate_right,false,35f)
+        GlobalScope.launch {
+            delay(500)
+            starRotation(star,800)
+        }
         GlobalScope.launch {
             delay(1500L)
             startActivity(Intent(this@SplashActivity, MainActivity::class.java))
@@ -35,7 +37,31 @@ class SplashActivity : AppCompatActivity() {
         }
     }
 
-    private fun gate_rotation(view: View, isLeft: Boolean,angleDelta: Float){
+    private fun starRotation(view:View,length: Long){
+        val rotate = RotateAnimation(0f,180f,0.5f,0.5f)
+        val alpha1 = AlphaAnimation(0f,1.0f)
+        val alpha2 = AlphaAnimation(1.0f,0f)
+        rotate.apply {
+            duration=length
+            fillAfter=true
+        }
+        alpha1.apply {
+            duration=length/2
+            fillAfter=true
+            interpolator=AccelerateInterpolator()
+        }
+        alpha2.apply {
+            duration=length/2
+            fillAfter=true
+            interpolator=AccelerateInterpolator()
+            startOffset=length/2
+        }
+        view.startAnimation(rotate)
+        view.startAnimation(alpha1)
+        view.startAnimation(alpha2)
+    }
+
+    private fun gateRotation(view: View, isLeft: Boolean, angleDelta: Float){
         val rotation = if (isLeft)
             Rotate3dAnimation(0f,angleDelta,0f,view.height/2.0f,0f,false)
         else
